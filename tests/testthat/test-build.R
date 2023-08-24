73cb916003e3e305d256bcc588c749554a7a6d88
Tadhg Moore <tadhg.moore6@gmail.com>
2023-08-24 12:45:49 -0400

    Remove redundant test

--- tests/testthat/test-build.R
+++ tests/testthat/test-build.R
@@ -131,26 +131,3 @@ test_that("building GOTM-WET works", {
                                     model, "fabm.yaml"))
   testthat::expect_true(file_chk)
 })
-
-test_that("building in parallel works", {
-  library(AEME)
-  tmpdir <- tempdir()
-  path <- file.path(tmpdir, "lake")
-  unlink(path, recursive = TRUE, force = TRUE)
-  aeme_dir <- system.file("extdata/lake/", package = "AEME")
-  # Copy files from package into tempdir
-  file.copy(aeme_dir, tmpdir, recursive = TRUE)
-  list.files(tmpdir, full.names = TRUE, recursive = TRUE)
-  aeme_data <- yaml_to_aeme(path = path, "aeme.yaml")
-  mod_ctrls <- read.csv(file.path(path, "model_controls.csv"))
-  inf_factor = c("dy_cd" = 1, "glm_aed" = 1, "gotm_wet" = 1)
-  outf_factor = c("dy_cd" = 1, "glm_aed" = 1, "gotm_wet" = 1)
-  model <- c("dy_cd", "glm_aed", "gotm_wet")
-  build_ensemble(path = path, aeme_data = aeme_data, model = model,
-                 mod_ctrls = mod_ctrls, inf_factor = inf_factor, ext_elev = 5,
-                 use_bgc = TRUE, use_lw = TRUE)
-  file_chk <- file.exists(file.path(path, paste0(aeme_data@lake$id,"_",
-                                                 tolower(aeme_data@lake$name)),
-                                    model, "fabm.yaml"))
-  testthat::expect_true(file_chk)
-})
