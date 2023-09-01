cc3222a3906770adf32fbd8a0976b29a01bb5173
Tadhg Moore <tadhg.moore6@gmail.com>
2023-09-01 09:28:21 -0400

    Moved input before output

--- R/rename_modelvars.R
+++ R/rename_modelvars.R
@@ -1,8 +1,8 @@
 #' Rename variables among the models and controlled vocabulary
 #'
 #' @param input string; vector of names
-#' @param type_output string; type of output
 #' @param type_input string; type of input
+#' @param type_output string; type of output
 #' @param verbose boolean; print changes to console
 #'
 #' @return vector of changed names
@@ -10,8 +10,8 @@
 #' @importFrom utils data
 
 rename_modelvars <- function(input,
-                             type_output = "name_parse",
                              type_input = "name",
+                             type_output = "name_parse",
                              verbose = F) {
 
   # Load Rdata
@@ -19,7 +19,7 @@ rename_modelvars <- function(input,
 
   this.key <- data.frame(key_naming[key_naming[, type_input] %in% input,])
 
-  names.new <- this.key[match(input,this.key[,type_input]), type_output]
+  names.new <- this.key[match(input, this.key[, type_input]), type_output]
 
   if (sum(is.na(names.new)) > 0) {
     stop("incomplete match for names.. check input")
