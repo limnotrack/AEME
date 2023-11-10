58278e00597c787a9133288671fb1aef70ca42b1
Tadhg Moore <tadhg.moore6@gmail.com>
2023-11-10 16:23:14 +1300

    Added test with minimal build

--- tests/testthat/test-limited-inputs.R
+++ tests/testthat/test-limited-inputs.R
@@ -33,6 +33,71 @@ test_that("it errors when met data is not present", {
     })
 })
 
+testthat::test_that("can build AEME with simple set of inputs", {
+
+  tmpdir <- tempdir()
+  aeme_dir <- system.file("extdata/lake/", package = "AEME")
+  # Copy files from package into tempdir
+  file.copy(aeme_dir, tmpdir, recursive = TRUE)
+  path <- file.path(tmpdir, "lake")
+
+  hyps <- read.csv(file.path(path, "data", "hypsograph.csv"))
+  met <- read.csv(file.path(path, "data", "meteo.csv"))
+
+  # Keep meteo
+  aeme_input <- list(
+    lake = list(
+      name = "Wainamu",
+      id = 45819,
+      latitude = -36.89,
+      longitude = 174.47,
+      elevation = 23.64,
+      depth = 13.07,
+      area = 152343
+    ),
+    time = list(
+      start = "2022-01-09 00:00:00",
+      stop = "2022-12-30 00:00:00",
+      time_step = 3600
+    ),
+    input = list(
+      init_depth = 13.0,
+      hypsograph = hyps,
+      meteo = met,
+      Kw = 0.98
+    )
+  )
+
+  aeme_data <- aeme_constructor(lake = aeme_input$lake, time = aeme_input$time,
+                                input = aeme_input$input)
+
+  plot(aeme_data, "input")
+
+  mod_ctrls <- read.csv(file.path(path, "model_controls.csv"))
+  inf_factor = c("dy_cd" = 1, "glm_aed" = 1, "gotm_wet" = 1)
+  outf_factor = c("dy_cd" = 1, "glm_aed" = 1, "gotm_wet" = 1)
+  model <- c("dy_cd", "glm_aed", "gotm_wet")
+
+  aeme_data <- build_ensemble(path = path, aeme_data = aeme_data, model = model,
+                              mod_ctrls = mod_ctrls, inf_factor = inf_factor,
+                              ext_elev = 5, use_bgc = FALSE)
+  inp <- input(aeme_data)
+
+  testthat::expect_true(is.data.frame(inp$hypsograph))
+
+  aeme_data <- run_aeme(aeme_data = aeme_data, model = model, verbose = TRUE,
+                        mod_ctrls = mod_ctrls, path = path)
+
+  lke <- lake(aeme_data)
+  file_chk <- all(file.exists(file.path(path, paste0(lke$id, "_",
+                                                     tolower(lke$name)),
+                                        model[1], "DYsim.nc")),
+                  file.exists(file.path(path, paste0(lke$id, "_",
+                                                     tolower(lke$name)),
+                                        model[2:3], "output", "output.nc")))
+  testthat::expect_true(file_chk)
+})
+
 testthat::test_that("can run AEME with simple set of inputs works", {
 
   tmpdir <- tempdir()
