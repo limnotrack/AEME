259c94baa0cbdd389b133fcbde8b24d32ca7901a
Tadhg Moore <tadhg.moore6@gmail.com>
2023-11-30 18:11:16 +1300

    Add tests for wbal method

--- tests/testthat/test-run.R
+++ tests/testthat/test-run.R
@@ -179,6 +179,109 @@ test_that("running models in parallel works", {
   testthat::expect_true(is.data.frame(model_performance))
 })
 
+test_that("running models with wbal method = 1", {
+  tmpdir <- tempdir()
+  aeme_dir <- system.file("extdata/lake/", package = "AEME")
+  # Copy files from package into tempdir
+  file.copy(aeme_dir, tmpdir, recursive = TRUE)
+  path <- file.path(tmpdir, "lake")
+  aeme_data <- yaml_to_aeme(path = path, "aeme.yaml")
+  mod_ctrls <- read.csv(file.path(path, "model_controls.csv"))
+  inf_factor = c("dy_cd" = 1, "glm_aed" = 1, "gotm_wet" = 1)
+  outf_factor = c("dy_cd" = 1, "glm_aed" = 1, "gotm_wet" = 1)
+  model <- c("dy_cd", "glm_aed", "gotm_wet")
+
+  w_bal <- water_balance(aeme_data)
+  w_bal$method <- 1
+  water_balance(aeme_data) <- w_bal
+
+  aeme_data <- build_ensemble(path = path, aeme_data = aeme_data, model = model,
+                              mod_ctrls = mod_ctrls, inf_factor = inf_factor,
+                              ext_elev = 5, use_bgc = FALSE, calc_wbal = T,
+                              calc_wlev = F)
+  inp <- input(aeme_data)
+  met <- inp$meteo
+  aeme_data <- run_aeme(aeme_data = aeme_data, model = model, verbose = TRUE,
+                        mod_ctrls = mod_ctrls, path = path, parallel = TRUE)
+  lke <- lake(aeme_data)
+  file_chk <- file.exists(file.path(path, paste0(lke$id, "_",
+                                                 tolower(lke$name)),
+                                    model[1], "DYsim.nc"))
+  testthat::expect_true(file_chk)
+
+  file_chk <- all(file.exists(file.path(path, paste0(lke$id, "_",
+                                                     tolower(lke$name)),
+                                        model[-1], "output", "output.nc")))
+  testthat::expect_true(file_chk)
+
+  model_performance <- assess_model(aeme_data = aeme_data, model = model,
+                                    var_sim = c("LKE_lvlwtr", "HYD_temp"))
+  testthat::expect_true(is.data.frame(model_performance))
+})
+
+test_that("running models with wbal method = 3", {
+  tmpdir <- tempdir()
+  aeme_dir <- system.file("extdata/lake/", package = "AEME")
+  # Copy files from package into tempdir
+  file.copy(aeme_dir, tmpdir, recursive = TRUE)
+  path <- file.path(tmpdir, "lake")
+  aeme_data <- yaml_to_aeme(path = path, "aeme.yaml")
+  mod_ctrls <- read.csv(file.path(path, "model_controls.csv"))
+  inf_factor = c("dy_cd" = 1, "glm_aed" = 1, "gotm_wet" = 1)
+  outf_factor = c("dy_cd" = 1, "glm_aed" = 1, "gotm_wet" = 1)
+  model <- c("dy_cd", "glm_aed", "gotm_wet")
+
+  w_bal <- water_balance(aeme_data)
+  w_bal$method <- 3
+  water_balance(aeme_data) <- w_bal
+
+  aeme_data <- build_ensemble(path = path, aeme_data = aeme_data, model = model,
+                              mod_ctrls = mod_ctrls, inf_factor = inf_factor,
+                              ext_elev = 5, use_bgc = FALSE, calc_wbal = T,
+                              calc_wlev = F, hum_type = 1)
+  aeme_data <- run_aeme(aeme_data = aeme_data, model = model, verbose = TRUE,
+                        mod_ctrls = mod_ctrls, path = path, parallel = TRUE)
+#
+#   plot_output(aeme_data, model)
+#   plot_output(aeme_data = aeme_data, model = model, var_sim = "LKE_lvlwtr",
+#               facet = F)
+#   plot_output(aeme_data = aeme_data, model = model, var_sim = "LKE_netwbl",
+#               facet = F, cumulative = F)
+#   plot_wbal(aeme_data = aeme_data)
+#
+#   tst <- get_var(aeme_data = aeme_data, model = model, var = "LKE_netwbl")
+
+
+  # w_bal <- water_balance(aeme_data)
+  #
+  # gotm_evap <- get_var(aeme_data = aeme_data, model = "dy_cd",
+  #                      var = "LKE_evpflx")
+  #
+  # ggplot() +
+  #   geom_line(data = w_bal$data$wbal, aes(x = Date, y = -dy_cd_evap_flux,
+  #                                         colour = "Est")) +
+  #   geom_line(data = gotm_evap, aes(x = Date, y = value))
+  #
+  # gotm_ts <- get_var(aeme_data = aeme_data, model = "dy_cd",
+  #                    var = "HYD_Ts")
+  #
+  # ggplot() +
+  #   geom_line(data = w_bal$data$wbal, aes(x = Date, y = Ts,
+  #                                         colour = "Est")) +
+  #   geom_line(data = gotm_ts, aes(x = Date, y = value))
+
+  lke <- lake(aeme_data)
+  file_chk <- file.exists(file.path(path, paste0(lke$id, "_",
+                                                 tolower(lke$name)),
+                                    model[1], "DYsim.nc"))
+  testthat::expect_true(file_chk)
+
+  file_chk <- all(file.exists(file.path(path, paste0(lke$id, "_",
+                                                     tolower(lke$name)),
+                                        model[-1], "output", "output.nc")))
+  testthat::expect_true(file_chk)
+})
+
 
 test_that("running models in parallel with no wbal calculated", {
   tmpdir <- tempdir()
