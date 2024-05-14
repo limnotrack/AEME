5a6032b55bd8f21f06acdfb4af8b86edc2c817b4
Tadhg Moore <tadhg.moore6@gmail.com>
2024-05-14 15:52:04 +0000

    Can no retrieve from aeme object

--- R/get_model_controls.R
+++ R/get_model_controls.R
@@ -11,25 +11,31 @@
 #' }
 #'
 
-get_model_controls <- function(use_bgc = FALSE) {
-  utils::data("model_controls", package = "AEME")
-  hyd_vars <- c("HYD_temp", "CHM_salt", "CHM_oxy")
-  bgc_vars <- c("PHS_frp", "PHS_dop", "PHS_pop", "PHS_pip", "PHS_tp",
-                "NIT_amm", "NIT_nit", "NIT_don", "NIT_pon", "NIT_tn",
-                "CAR_doc", "CAR_poc",
-                "SIL_rsi",
-                "PHY_cyano", "PHY_green", "PHY_crypt", "PHY_diatom",
-                "PHY_tchla",
-                "NCS_ss1"
-                )
-  if (use_bgc) {
-    model_controls <- model_controls |>
-      dplyr::mutate(simulate = ifelse(var_aeme %in% c(hyd_vars, bgc_vars),
-                                      TRUE, FALSE))
+get_model_controls <- function(aeme = NULL, use_bgc = FALSE) {
 
+  if (!is.null(aeme)) {
+    config <- configuration(aeme)
+    model_controls <- config$model_controls
   } else {
-    model_controls <- model_controls |>
-      dplyr::mutate(simulate = ifelse(var_aeme %in% c(hyd_vars), TRUE, FALSE))
+    utils::data("model_controls", package = "AEME")
+    hyd_vars <- c("HYD_temp", "CHM_salt", "CHM_oxy")
+    bgc_vars <- c("PHS_frp", "PHS_dop", "PHS_pop", "PHS_pip", "PHS_tp",
+                  "NIT_amm", "NIT_nit", "NIT_don", "NIT_pon", "NIT_tn",
+                  "CAR_doc", "CAR_poc",
+                  "SIL_rsi",
+                  "PHY_cyano", "PHY_green", "PHY_crypt", "PHY_diatom",
+                  "PHY_tchla",
+                  "NCS_ss1"
+    )
+    if (use_bgc) {
+      model_controls <- model_controls |>
+        dplyr::mutate(simulate = ifelse(var_aeme %in% c(hyd_vars, bgc_vars),
+                                        TRUE, FALSE))
+
+    } else {
+      model_controls <- model_controls |>
+        dplyr::mutate(simulate = ifelse(var_aeme %in% c(hyd_vars), TRUE, FALSE))
+    }
   }
   return(model_controls)
 }
