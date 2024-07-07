ccc568a84f40321786a9ed0ef9a0bbb98ef7cdd8
Tadhg Moore <tadhg.moore6@gmail.com>
2024-07-07 22:53:33 +0000

    Fun to remove aeme output

---
+++ R/remove_output.R
@@ -0,0 +1,13 @@
+#' Remove output from AEME object
+#'
+#' @inheritParams build_aeme
+#'
+#' @inherit build_aeme return
+#' @export
+#'
+
+remove_output <- function(aeme) {
+  outp <- output(aeme)
+  outp <- list(n_members = 0)
+  output(aeme) <- outp
+}
