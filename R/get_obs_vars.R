ede2dd37c05cff08c484b45c047053676aa89a5b
Tadhg Moore <tadhg.moore6@gmail.com>
2024-10-01 10:06:41 +1300

    Get vars in obs and output

---
+++ R/get_obs_vars.R
@@ -0,0 +1,36 @@
+#' Get observation variable names
+#' 
+#' @inheritParams build_aeme
+#' 
+#' @return Observation variable names vector
+#' 
+#' @export
+#' 
+#' @importFrom utils data
+#' @importFrom dplyr bind_rows filter pull
+#' 
+#' @examples
+#' aeme_file <- system.file("extdata/aeme.rds", package = "AEME")
+#' aeme <- readRDS(aeme_file)
+#' get_obs_vars(aeme)
+
+get_obs_vars <- function(aeme) {
+  utils::data("key_naming", package = "AEME")
+  obs_vars <- AEME::observations(aeme) |> 
+    dplyr::bind_rows()
+  
+  if (nrow(obs_vars) == 0) {
+    return()
+  }
+  
+  obs_vars <- obs_vars |>
+    dplyr::filter(!is.na(var_aeme)) |> 
+    dplyr::pull(var_aeme) |> 
+    unique()
+  if (length(obs_vars) == 0) {
+    return()
+  }
+  name <- key_naming$name_text[match(obs_vars, key_naming$name)]
+  idx <- !is.na(name)
+  setNames(obs_vars[idx], name[idx])
+}
---
+++ R/get_output_vars.R
@@ -0,0 +1,51 @@
+#' Get the output variables from an AEME object
+#'
+#' @inheritParams build_aeme
+#'
+#' @return A character vector of the output variables
+#' @export
+#'
+#' @examples
+#' aeme_file <- system.file("extdata/aeme.rds", package = "AEME")
+#' aeme <- readRDS(aeme_file)
+#' path <- tempdir()
+#' model_controls <- get_model_controls(use_bgc = TRUE)
+#' model <- c("glm_aed", "gotm_wet")
+#' aeme <- build_aeme(path = path, aeme = aeme, model = model,
+#'                    model_controls = model_controls,
+#'                    ext_elev = 5, use_bgc = TRUE)
+#' # Run models
+#' aeme <- run_aeme(aeme = aeme, model = model, verbose = FALSE,
+#' path = path, model_controls = model_controls,
+#' parallel = TRUE, ncores = 2L)
+#' get_output_vars(aeme)
+
+get_output_vars <- function(aeme, n_ens = 1) {
+  
+  outp <- AEME::output(aeme)
+  ens_label <- paste0("ens_", sprintf("%03d", n_ens))
+  out <- outp[[ens_label]]
+  mod_vars <- names(out$glm_aed)
+  
+  # Loop through the variables and get the ones that are not all -99
+  out_vars <- c()
+  for (i in 1:length(mod_vars)) {
+    if (!all(out$glm_aed[[mod_vars[i]]] == -99)) {
+      out_vars <- c(out_vars, mod_vars[i])
+    }
+  }
+  utils::data("key_naming", package = "AEME")
+  out_var_names <- key_naming$name_text[match(out_vars, key_naming$name)]
+  nmes <- setNames(out_vars, out_var_names)
+  nmes <- nmes[!is.na(nmes)]
+  nmes <- nmes[!is.na(names(nmes)) & names(nmes) != ""]
+  nmes <- nmes[-1]
+  
+  # order variables with target variables first
+  tgt_vars <- c("HYD_temp", "HYD_thmcln", "CHM_oxy", "PHY_tchla",
+                "NIT_tn", "PHS_tp")
+  
+  nmes <- nmes[order(match(nmes, tgt_vars))]
+  
+  return(nmes)
+}
