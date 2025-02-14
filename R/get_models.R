584863110f9cf822616056b2f125439f0b024b1c
Tadhg Moore <tadhg.moore6@gmail.com>
2025-02-14 17:01:53 +1300

    Add fun to get model names

---
+++ R/get_models.R
@@ -0,0 +1,19 @@
+#' Get a vector of available models
+#'
+#' @inheritParams build_aeme
+#'
+#' @returns vector of available models
+#' @export
+#'
+
+get_models <- function(aeme = NULL) {
+  models <- c("DYRESM-CAEDYM" = "dy_cd", "GLM-AED" = "glm_aed",
+              "GOTM-WET" = "gotm_wet")
+
+  if (!is.null(aeme)) {
+    cfg <- configuration(aeme)
+    chk <- sapply(models, \(m) !is.null(cfg[[m]][["hydrodynamic"]]))
+    models <- models[chk]
+  }
+  return(models)
+}
