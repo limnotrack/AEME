c9010cd91b7e913c8931965e795af9f56d8e0201
tadhg-moore <tadhg.moore6@gmail.com>
2025-10-22 11:41:57 +1300

    Add catch for filling GLM inflows

--- R/aeme_to_inflow.R
+++ R/aeme_to_inflow.R
@@ -12,6 +12,14 @@
 aeme_to_inflow <- function(aeme) {
   aeme <- check_aeme(aeme)
   model <- list_models(aeme = aeme)
+  tme <- time(aeme)
+  spin_up <- lapply(model, \(m) {
+    list(start = lubridate::as_date(tme$start - 
+                                      lubridate::ddays(tme$spin_up[[m]])),
+         end = lubridate::as_date(tme$stop)
+         )
+  })
+  names(spin_up) <- model
   outflow_factor <- rep(1, length(model))
   names(outflow_factor) <- model
   
@@ -43,6 +51,30 @@ aeme_to_inflow <- function(aeme) {
     df <- lapply(inflow_vars, \(var) {
       dat <- get_var(aeme = aeme, model = m, var_sim = var, depth = 0.25,
                      remove_spin_up = FALSE)
+      if (m == "glm_aed") {
+        # Add extra dates for spin-up period
+        dat_extra <- dat |>
+          dplyr::filter(Date >= spin_up[[m]]$start & 
+                          Date < lubridate::as_date(tme$start))
+        if (nrow(dat_extra) < tme$spin_up[[m]]) {
+          # Add missing dates with mean of the first 3 values
+          missing_dates <- seq.Date(from = spin_up[[m]]$start,
+                                    to = lubridate::as_date(tme$start) - 1,
+                                    by = "day")
+          missing_dates <- missing_dates[!missing_dates %in% dat_extra$Date]
+          if (length(missing_dates) > 0) {
+            mean_value <- dat |>
+              dplyr::filter(Date >= lubridate::as_date(tme$start) &
+                              Date < lubridate::as_date(tme$start) + 3) |>
+              dplyr::pull(value) |> 
+              mean(na.rm = TRUE)
+            dat_missing <- data.frame(Date = missing_dates,
+                                      var_sim = var,
+                                      value = mean_value)
+            dat <- dplyr::bind_rows(dat_missing, dat)
+          }
+        }
+      }
       if (var == "LKE_outflow") {
         dat <- dat |> 
           dplyr::mutate(value = value * outflow_factor[m])
