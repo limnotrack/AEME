dbedb21208931868d69f24101de36e1b46b6f00e
tadhg-moore <tadhg.moore6@gmail.com>
2025-10-22 01:53:37 +0000

    Add day 0 row and format as long df

--- R/aeme_to_inflow.R
+++ R/aeme_to_inflow.R
@@ -46,35 +46,26 @@ aeme_to_inflow <- function(aeme) {
     "NIT_don", "NIT_pon", "PHS_dop", "PHS_pop", "PHS_pip",
     "CAR_doc", "CAR_poc"
   )
+  var_check <- check_var_in_output(aeme = aeme, model = model, 
+                                   var_sim = inflow_vars)
   
   out <- lapply(model, \(m) {
     df <- lapply(inflow_vars, \(var) {
+      
+      if (var_check[[m]][var] == FALSE) {
+        return(NULL)
+      }
       dat <- get_var(aeme = aeme, model = m, var_sim = var, depth = 0.25,
                      remove_spin_up = FALSE)
-      if (m == "glm_aed") {
-        # Add extra dates for spin-up period
-        dat_extra <- dat |>
-          dplyr::filter(Date >= spin_up[[m]]$start & 
-                          Date < lubridate::as_date(tme$start))
-        if (nrow(dat_extra) < tme$spin_up[[m]]) {
-          # Add missing dates with mean of the first 3 values
-          missing_dates <- seq.Date(from = spin_up[[m]]$start,
-                                    to = lubridate::as_date(tme$start) - 1,
-                                    by = "day")
-          missing_dates <- missing_dates[!missing_dates %in% dat_extra$Date]
-          if (length(missing_dates) > 0) {
-            mean_value <- dat |>
-              dplyr::filter(Date >= lubridate::as_date(tme$start) &
-                              Date < lubridate::as_date(tme$start) + 3) |>
-              dplyr::pull(value) |> 
-              mean(na.rm = TRUE)
-            dat_missing <- data.frame(Date = missing_dates,
-                                      var_sim = var,
-                                      value = mean_value)
-            dat <- dplyr::bind_rows(dat_missing, dat)
-          }
-        }
-      }
+      mean_value <- dat |>
+        dplyr::filter(Date >= lubridate::as_date(tme$start) &
+                        Date < lubridate::as_date(tme$start) + 3) |>
+        dplyr::pull(value) |> 
+        mean(na.rm = TRUE)
+      dat_missing <- data.frame(Date = (dat$Date[1] - lubridate::ddays(1)),
+                                var_sim = var,
+                                value = mean_value)
+      dat <- dplyr::bind_rows(dat_missing, dat)
       if (var == "LKE_outflow") {
         dat <- dat |> 
           dplyr::mutate(value = value * outflow_factor[m])
@@ -92,7 +83,7 @@ aeme_to_inflow <- function(aeme) {
       dplyr::rename(HYD_flow = LKE_outflow) |>
       dplyr::mutate(model = m)
     return(df_wid)
-  }) 
-  names(out) <- model
+  }) |> 
+    dplyr::bind_rows()
   return(out)
 }
