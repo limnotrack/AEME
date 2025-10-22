b271f1a6e62ea04e89fae0b8a9572f3e0c97ef97
tadhg-moore <tadhg.moore6@gmail.com>
2025-10-23 12:05:49 +1300

    Update dy_cd params

--- data-raw/aeme_parameters.R
+++ data-raw/aeme_parameters.R
@@ -1,6 +1,6 @@
 param_names <- AEME:::get_param_names()
 aeme_parameters <- read.csv("data-raw/aeme_parameters.csv") |>
   dplyr::mutate(group = NA)|>
-  dplyr::select(dplyr::all_of(c(param_names)))
+  dplyr::select(dplyr::any_of(c(param_names)))
 
 usethis::use_data(aeme_parameters, overwrite = TRUE)
--- data-raw/aeme_parameters_bgc.R
+++ data-raw/aeme_parameters_bgc.R
@@ -3,6 +3,6 @@
 param_names <- AEME:::get_param_names()
 aeme_parameters_bgc <- read.csv("data-raw/aeme_parameters_bgc.csv") |>
   dplyr::mutate(group = NA) |>
-  dplyr::select(dplyr::all_of(c(param_names)))
+  dplyr::select(dplyr::any_of(c(param_names)))
 
 usethis::use_data(aeme_parameters_bgc, overwrite = TRUE)
