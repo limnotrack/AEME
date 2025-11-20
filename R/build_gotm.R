d3f8bd000d7a9785b400e1f00ec7d0a8bda7cb42
tadhg-moore <tadhg.moore6@gmail.com>
2025-11-20 14:07:39 +1300

    move checks to build_aeme

--- R/build_gotm.R
+++ R/build_gotm.R
@@ -67,5 +67,6 @@ build_gotm <- function(lakename, model_controls, date_range,
                          use_bgc = use_bgc, model_controls = model_controls)
 
   if (overwrite_yaml) write_yaml(gotm, gotm_file)
-  check_gotm_yaml(file = gotm_file)
+  return(gotm_file)
+  # check_gotm_yaml(file = gotm_file)
 }
