cec03194963cb986e30cf21fa989499c9ad8ca0e
tadhg-moore <tadhg.moore6@gmail.com>
2025-11-20 14:09:37 +1300

    Add all dyresm files

--- R/get_model_config_files.R
+++ R/get_model_config_files.R
@@ -1,23 +1,26 @@
 #' Get model configuration files paths
 #'
 #' @inheritParams build_aeme
-#'
+#' @param lake_dir Path to the lake AEME directory. If NULL, it will be
+#' computed from `aeme` and `path`.
 #' @returns A list with model configuration files paths
 #' @export
 #'
 
-get_model_config_files <- function(aeme, model, path) {
+get_model_config_files <- function(aeme, model, path, lake_dir = NULL) {
   
-  aeme <- check_aeme(aeme)
   model <- check_model(model = model)
-  path <- check_path(path = path, must_exist = TRUE)
-  lake_dir <- get_lake_dir(aeme = aeme, path = path)
+  if (is.null(lake_dir)) {
+    aeme <- check_aeme(aeme)
+    path <- check_path(path = path, must_exist = TRUE)
+    lake_dir <- get_lake_dir(aeme = aeme, path = path)
+  }
   
   out <- list()
   
   if ("glm_aed" %in% model) {
     glm_files <- list.files(
-      path = file.path(lake_dir, "glm_aed"),
+      path = file.path(lake_dir),
       pattern = "\\.nml$",
       full.names = TRUE,
       recursive = TRUE
@@ -28,9 +31,10 @@ get_model_config_files <- function(aeme, model, path) {
   
   if ("gotm_wet" %in% model) {
     gotm_files <- list.files(
-      path = file.path(lake_dir, "gotm_wet"),
+      path = file.path(lake_dir),
       pattern = "\\.yaml$",
-      full.names = TRUE
+      full.names = TRUE,
+      recursive = TRUE
     )
     names(gotm_files) <- basename(tools::file_path_sans_ext(gotm_files))
     out$gotm_wet <- gotm_files
@@ -38,9 +42,10 @@ get_model_config_files <- function(aeme, model, path) {
   
   if ("dy_cd" %in% model) {
     dycd_files <- list.files(
-      path = file.path(lake_dir, "dy_cd"),
-      pattern = "\\.(bio|chm|sed|par)$",
-      full.names = TRUE
+      path = file.path(lake_dir),
+      pattern = "\\.(bio|chm|sed|par|cfg|con|inf|met|pro|stg|wdr)$",
+      full.names = TRUE,
+      recursive = TRUE
     )
     names(dycd_files) <- tools::file_ext(basename(dycd_files))
     out$dy_cd <- dycd_files
