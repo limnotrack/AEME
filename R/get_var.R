8f819609b17a7b5c92799f3832b24ce21d606e1b
tadhg-moore <tadhg.moore6@gmail.com>
2025-11-20 14:20:35 +1300

    Update with new depths format

--- R/get_var.R
+++ R/get_var.R
@@ -14,7 +14,6 @@
 #'
 #' @importFrom dplyr arrange filter left_join mutate select bind_rows case_when
 #' rename
-#' @importFrom stats approx
 #'
 #' @return dataframe or list
 #' @export
@@ -38,6 +37,8 @@ get_var <- function(aeme, model, var_sim, depth = NULL, return_df = TRUE,
   max_dep <- max(bathy$depth)
   outp <- output(aeme)
   aeme_time <- time(aeme)
+  date_index <- get_date_index(aeme = aeme, model = model,
+                               remove_spin_up = remove_spin_up)
   names(model) <- model
   ens_lab <- format_ens_label(ens_n = ens_n)
   
@@ -45,8 +46,8 @@ get_var <- function(aeme, model, var_sim, depth = NULL, return_df = TRUE,
     obs <- observations(aeme)
     if (var_sim == "LKE_lvlwtr") {
       if (is.null(obs$level)) {
-        message(strwrap("No lake level observations found. Using bathymetry
-                        [depth = 0 m] as lake level.", width = 80))
+        cli_inform_safe(c("i" = "No lake level observations found. Using bathymetry
+                        [depth = 0 m] as lake level."))
         obs_sub <- data.frame(Date = seq.Date(as.Date(aeme_time$start),
                                               as.Date(aeme_time$stop),
                                               by = 1),
@@ -64,12 +65,12 @@ get_var <- function(aeme, model, var_sim, depth = NULL, return_df = TRUE,
       obs_sub <- obs$lake |>
         dplyr::filter(Date >= aeme_time$start & Date <= aeme_time$stop &
                         var_aeme %in% var_sim) |>
-        dplyr::mutate(depth_mid = (depth_from + depth_to) / 2) |>
-        dplyr::arrange(Date, depth_mid) |>
-        dplyr::select(Date, var_aeme, depth_mid, value)
+        dplyr::mutate(depth = (depth_from + depth_to) / 2) |>
+        dplyr::arrange(Date, depth) |>
+        dplyr::select(Date, var_aeme, depth, value)
     }
     if (nrow(obs_sub) == 0) {
-      stop("No observations found for the model simulation period.")
+      cli::cli_abort("No observations found for the model simulation period.")
     }
     obs_sub <- obs_sub |>
       dplyr::rename(obs = value)
@@ -77,9 +78,14 @@ get_var <- function(aeme, model, var_sim, depth = NULL, return_df = TRUE,
   
   # Loop through the models and extract the variable of interest ----
   lst <- lapply(model, \(m) {
+    lapply(outp[[ens_lab]][[m]], dim)
+    
     variable <- outp[[ens_lab]][[m]][[var_sim]]
     # Empty dataframe to return if variable is not in output
     df <- data.frame(Date = as.Date(NA),
+                     depth = NA_real_,
+                     value = NA_real_, 
+                     var_sim = var_sim,
                      lyr_top = NA,
                      value = NA,
                      Model = toggle_models(m, to = "display"),
@@ -103,21 +109,31 @@ get_var <- function(aeme, model, var_sim, depth = NULL, return_df = TRUE,
       return(df)
     }
     
+          # Build long dataframe for 2D variable
+      # each <- nrow(outp[[ens_lab]][[m]][[var_sim]])
+      # data.frame(
+      #   var_sim = var_sim,
+      #   Date     = rep(outp[[ens_lab]][[m]][["Date"]], each = each),
+      #   depth    = as.vector(outp[[ens_lab]][[m]][["LKE_depths"]]),
+      #   value    = as.vector(outp[[ens_lab]][[m]][[var_sim]]),
+      #   stringsAsFactors = FALSE
+      # )
+    
     if (use_obs) {
       
       obs_dates <- unique(obs_sub$Date)
-      date_ind <- which(outp[[ens_lab]][[m]][["Date"]] %in% obs_dates)
+      date_index <- which(outp[[ens_lab]][[m]][["Date"]] %in% obs_dates)
       
       if (var_sim == "LKE_lvlwtr") {
         
-        df <- data.frame(Date = outp[[ens_lab]][[m]][["Date"]][date_ind],
-                         sim = outp[[ens_lab]][[m]][["LKE_lvlwtr"]][date_ind] +
+        df <- data.frame(Date = outp[[ens_lab]][[m]][["Date"]][date_index],
+                         sim = outp[[ens_lab]][[m]][["LKE_lvlwtr"]][date_index] +
                            min(inp$hypsograph$elev),
                          Model = toggle_models(m, to = "display")) |>
           dplyr::left_join(obs_sub, by = c("Date" = "Date"))
       } else if (is.vector(variable)) {
-        df <- data.frame(Date = outp[[ens_lab]][[m]][["Date"]][date_ind],
-                         sim = outp[[ens_lab]][[m]][[var_sim]][date_ind],
+        df <- data.frame(Date = outp[[ens_lab]][[m]][["Date"]][date_index],
+                         sim = outp[[ens_lab]][[m]][[var_sim]][date_index],
                          Model = toggle_models(m, to = "display")) |>
           dplyr::left_join(obs_sub, by = c("Date" = "Date")) |>
           dplyr::mutate(
@@ -128,35 +144,62 @@ get_var <- function(aeme, model, var_sim, depth = NULL, return_df = TRUE,
           )
       } else {
         
-        mod <- lapply(date_ind, \(d) {
-          depth <- outp[[ens_lab]][[m]][["LKE_depths"]][, d]
-          v <- outp[[ens_lab]][[m]][[var_sim]][, d]
-          obs_deps <- unique(obs_sub$depth_mid[obs_sub$Date == outp[[ens_lab]][[m]][["Date"]][d]])
-          
-          if (all(is.na(v)) | all(is.na(depth))) {
-            return(data.frame(Date = outp[[ens_lab]][[m]][["Date"]][d],
-                              depth_mid = obs_deps,
-                              sim = NA,
-                              Model = toggle_models(m, to = "display")))
-          }
-          p <- stats::approx(depth, v, obs_deps, rule = 2)$y
-          data.frame(Date = outp[[ens_lab]][[m]][["Date"]][d],
-                     depth_mid = obs_deps,
-                     sim = p,
-                     Model = toggle_models(m, to = "display"))
-        }) |>
-          dplyr::bind_rows()
+        depth <- obs_sub |> 
+          dplyr::distinct(depth) |> 
+          dplyr::arrange(depth) |>
+          dplyr::pull(depth)
+        out_depths <- matrix(depth, nrow = length(depth),
+                             ncol = length(date_index))
+        depths <- outp[[ens_lab]][[m]][["LKE_depths"]][, date_index]
         
-        df <- dplyr::left_join(obs_sub, mod, by = c("Date" = "Date",
-                                                    "depth_mid" = "depth_mid"))
+        value <- interp_static_grid(var = variable[, date_index],
+                                    midpoints = depths,
+                                    out_depths = out_depths)
+        # Build long dataframe for 2D variable
+        each <- length(depth)
+        mod <- data.frame(
+          Date     = rep(outp[[ens_lab]][[m]][["Date"]][date_index], each = each),
+          depth    = as.vector(out_depths),
+          sim    = as.vector(value),
+          Model = toggle_models(m, to = "display"),
+          stringsAsFactors = FALSE
+        )
+        
+        
+        # df <- data.frame(Date = outp[[ens_lab]][[m]][["Date"]],
+        #                  depth = depth,
+        #                  value = value,
+        #                  Model = toggle_models(m, to = "display")
+        # )
+        # 
+        # mod <- lapply(date_index, \(d) {
+        #   depth <- outp[[ens_lab]][[m]][["LKE_depths"]][, d]
+        #   v <- outp[[ens_lab]][[m]][[var_sim]][, d]
+        #   obs_deps <- unique(obs_sub$depth_mid[obs_sub$Date == outp[[ens_lab]][[m]][["Date"]][d]])
+        #   
+        #   if (all(is.na(v)) | all(is.na(depth))) {
+        #     return(data.frame(Date = outp[[ens_lab]][[m]][["Date"]][d],
+        #                       depth_mid = obs_deps,
+        #                       sim = NA,
+        #                       Model = toggle_models(m, to = "display")))
+        #   }
+        #   # p <- stats::approx(depth, v, obs_deps, rule = 2)$y
+        #   data.frame(Date = outp[[ens_lab]][[m]][["Date"]][d],
+        #              depth_mid = obs_deps,
+        #              sim = p,
+        #              Model = toggle_models(m, to = "display"))
+        # }) |>
+        #   dplyr::bind_rows()
+        
+        df <- dplyr::left_join(obs_sub, mod, by = c("Date", "depth"))
         
       }
     } else if (is.null(dim(variable))) {
       df <- data.frame(Date = outp[[ens_lab]][[m]][["Date"]],
-                       lyr_top = NA,
+                       depth = NA_real_,
                        value = variable,
-                       Model = toggle_models(m, to = "display"),
-                       lyr_thk = NA)
+                       Model = toggle_models(m, to = "display")
+                       )
       # Trim off the spin up period ----
       if (remove_spin_up) {
         idx2 <- which(df$Date >= aeme_time$start & df$Date <= aeme_time$stop)
@@ -167,38 +210,37 @@ get_var <- function(aeme, model, var_sim, depth = NULL, return_df = TRUE,
           dplyr::mutate(value = cumsum(value))
       }
     } else {
-      dep <- data.frame(Date = outp[[ens_lab]][[m]][["Date"]],
-                        depth = outp[[ens_lab]][[m]][["LKE_lvlwtr"]])
-      lyr <- outp[[ens_lab]][[m]][["LKE_layers"]]
+      
+      lake_level <- outp[[ens_lab]][[m]][["LKE_lvlwtr"]]
+      depths <- outp[[ens_lab]][[m]][["LKE_depths"]]
+      # dep <- data.frame(Date = outp[[ens_lab]][[m]][["Date"]],
+      #                   depth = outp[[ens_lab]][[m]][["LKE_lvlwtr"]])
+      # lyr <- outp[[ens_lab]][[m]][["LKE_layers"]]
       if (!is.null(depth)) {
         min_depth <- 0
-        max_depth <- round(max(dep$depth), 2)
+        max_depth <- round(max(lake_level), 2)
         if (depth > max_depth | depth < min_depth) {
           stop(strwrap(paste0("Depth is outside the range of the modelled lake
                               levels [", min_depth, ", ", max_depth, "m]")))
         }
-        value <- sapply(1:ncol(variable), \(i) {
-          if (all(is.na(variable[, i]))) {
-            return(NA)
-          }
-          p <- stats::approx((dep$depth[i] - lyr[, i]), variable[, i], depth,
-                             rule = 2)$y
-          return(p)
-        })
+        out_depths <- matrix(depth, nrow = 1, ncol = ncol(variable))
+        value <- interp_static_grid(var = variable, midpoints = depths,
+                                    out_depths = out_depths)
         df <- data.frame(Date = outp[[ens_lab]][[m]][["Date"]],
-                         lyr_top = depth,
+                         depth = depth,
                          value = value,
-                         Model = toggle_models(m, to = "display"),
-                         lyr_thk = NA)
+                         Model = toggle_models(m, to = "display")
+                         )
       } else {
-        df <- data.frame(Date = rep(outp[[ens_lab]][[m]][["Date"]],
-                                    each = nrow(variable)),
-                         lyr_top = as.vector(lyr),
-                         value = as.vector(variable),
-                         Model = toggle_models(m, to = "display")) |>
-          dplyr::mutate(lyr_thk = ifelse(c(-999, diff(lyr_top)) < 0, # | is.na(-c(NA,diff(lyr_top))),
-                                         c(diff(lyr_top),NA),
-                                         c(NA,diff(lyr_top))))
+        # Build long dataframe for 2D variable
+        each <- nrow(variable)
+        df <- data.frame(
+          Date     = rep(outp[[ens_lab]][[m]][["Date"]], each = each),
+          depth    = as.vector(outp[[ens_lab]][[m]][["LKE_depths"]]),
+          value    = as.vector(outp[[ens_lab]][[m]][[var_sim]]),
+          Model = toggle_models(m, to = "display"),
+          stringsAsFactors = FALSE
+        )
       }
       
       
