12b660290a00df588cdab3f8c80e8955e2b1af1c
tadhg-moore <tadhg.moore6@gmail.com>
2025-12-05 10:58:29 +1300

    Add new fun for reading model config

---
+++ R/read_model_config.R
@@ -0,0 +1,56 @@
+#' Read in model configuration files for a given model and lake directory
+#'
+#' @param model character; model name ("dy_cd", "glm_aed", "gotm_wet"). Only
+#'  one model at a time.
+#' @param path character; directory which contains the model configuration files.
+#'
+#' @returns List with model configuration components. This includes a 'physical'
+#' list with physical model configuration and a 'bgc' list with biogeochemistry
+#' model configuration (if applicable).
+#' @export
+#'
+
+read_model_config <- function(model, path) {
+  model <- check_model(model)
+  path <- check_path(path, must_exist = TRUE)
+  out <- list()
+  if (length(model) != 1) {
+    cli::cli_abort("Please provide only one model at a time. {.arg model} has 
+                   {length(model)} models {.val {model}}.")
+  }
+  model_cfg_files <- get_model_config_files(lake_dir = path, 
+                                            model = model)[[model]]
+  cfg <- lapply(model_cfg_files, \(f) {
+    file_type <- tools::file_ext(f)
+    if (file_type == "nml") {
+      read_nml(f)
+    } else if (file_type %in% c("csv", "tsv")) {
+      read_aed_param_csv(f)
+    } else if (file_type == "yaml") {
+      yaml::read_yaml(file = f)
+    } else {
+      readLines(f)
+    }
+  })
+  
+  if (model == "dy_cd") {
+    out$physical <- list(par = cfg$par, cfg = cfg$cfg)
+    # Remove par and cfg from list
+    cfg$par <- NULL
+    cfg$cfg <- NULL
+  } else if (model == "glm_aed") {
+    out$physical <- cfg[["glm3"]]
+    # Remove glm3 from list
+    cfg[["glm3"]] <- NULL
+  } else if (model == "gotm_wet") {
+    out$physical <- list(gotm = cfg[["gotm"]],
+                         output = cfg[["output"]])
+    # Remove gotm and output from list
+    cfg[["gotm"]] <- NULL
+    cfg[["output"]] <- NULL
+  }
+  if (length(cfg) > 0) {
+    out$bgc <- cfg
+  }
+  return(out)
+}
