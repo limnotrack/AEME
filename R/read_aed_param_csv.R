a89641f93605c8787ea7d604632e318ccb1952ca
tadhg-moore <tadhg.moore6@gmail.com>
2025-12-04 15:01:06 +1300

    Read/write AED param csv

---
+++ R/read_aed_param_csv.R
@@ -0,0 +1,36 @@
+#' Read and write AED parameter CSV files
+#' These functions handle reading and writing AED parameter CSV files,
+#' ensuring that single quotes are properly managed in column names and
+#' specific columns.
+#' @param file The path to the CSV file.
+#' @param df A data frame to be written to CSV.
+#' @return For `read_aed_param_csv`, a data frame read from the CSV file.
+#' @export
+read_aed_param_csv <- function(file) {
+  # Check file exists
+  if (!file.exists(file)) {
+    cli::cli_abort(c("x" = "File {file} does not exist"))
+  }
+  
+  df <- readr::read_csv(file, show_col_types = FALSE)
+  
+  # Strip "'" from column names and column which has name in the column name
+  names(df) <- gsub("'", "", names(df))
+  name_col <- names(df)[grepl("name", names(df))]
+  if (length(name_col) > 0) {
+    df[[name_col]] <- gsub("'", "", df[[name_col]])
+  }
+  return(df)
+}
+
+#' @export
+#' @rdname read_aed_param_csv
+write_aed_param_csv <- function(df, file) {
+  # Add "'" to column names and column which has name in the column name
+  names(df) <- paste0("'", names(df), "'")
+  name_col <- names(df)[grepl("name", names(df))]
+  if (length(name_col) > 0) {
+    df[[name_col]] <- paste0("'", df[[name_col]], "'")
+  }
+  readr::write_csv(df, file)
+}
