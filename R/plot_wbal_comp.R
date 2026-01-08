04d8743d35b1f6b67aeeb377c02c713a9b0fd558
tadhg-moore <tadhg.moore6@gmail.com>
2026-01-09 11:12:37 +1300

    Comparison plot of wbal components

---
+++ R/plot_wbal_comp.R
@@ -0,0 +1,135 @@
+#' Plot Water Balance Comparison
+#'
+#' @inheritParams plot_wbal_summaries
+#'
+#' @returns ggplot object
+#' @export
+#'
+plot_wbal_comp <- function(wbal) {
+  
+  wb      <- wbal$wb
+  mod     <- wbal$mod
+
+  ## --- Lake level ---
+  comp <-  wb |> 
+    dplyr::select(Date, model, level) |>
+    dplyr::rename(est = level) |>
+    dplyr::left_join(mod$level, by = c("Date", "model")) |> 
+    dplyr::mutate(Model = toggle_models(model))
+  lims <- range(comp$est, comp$value, na.rm = TRUE)
+  p1 <- ggplot(comp) +
+    geom_point(aes(x = est, y = value)) +
+    geom_smooth(aes(x = est, y = value), method = "lm", se = FALSE, color = "blue") +
+    geom_abline(slope = 1, intercept = 0, color = "red") +
+    coord_equal(xlim = lims, ylim = lims) +
+    facet_wrap(~Model) +
+    labs(title = "Lake Level Comparison", x = "Estimated Level (m)", y = "Model Level (m)") +
+    theme_bw()
+  ## --- Total inflow ---
+  comp <- wb |> 
+    dplyr::select(Date, model, HYD_flow) |>
+    dplyr::rename(est = HYD_flow) |>
+    dplyr::left_join(mod$inflow, by = c("Date", "model")) |> 
+    dplyr::mutate(Model = toggle_models(model)) |> 
+    dplyr::filter(est > 0 & value > 0)
+  
+  p2 <- ggplot(comp) +
+    geom_point(aes(x = est, y = value)) +
+    geom_smooth(aes(x = est, y = value), method = "lm", se = FALSE, color = "blue") +
+    geom_abline(slope = 1, intercept = 0, color = "red") +
+    coord_equal(xlim = range(comp$est, comp$value, na.rm = TRUE),
+                ylim = range(comp$est, comp$value, na.rm = TRUE)) +
+    scale_x_log10() +
+    scale_y_log10() +
+    facet_wrap(~Model) +
+    labs(title = "Total Inflow Comparison", x = "Estimated Inflow (m³)", y = "Model Inflow (m³)") +
+    theme_bw()
+  
+  ## --- Total outflow ---
+  comp <- wb |> 
+    dplyr::select(Date, model, outflow) |>
+    dplyr::rename(est = outflow) |>
+    dplyr::left_join(mod$outflow, by = c("Date", "model")) |> 
+    dplyr::mutate(Model = toggle_models(model)) |> 
+    dplyr::filter(est > 0 & value > 0)
+  p3 <- ggplot(comp) +
+    geom_point(aes(x = est, y = value)) +
+    geom_smooth(aes(x = est, y = value), method = "lm", se = FALSE, color = "blue") +
+    geom_abline(slope = 1, intercept = 0, color = "red") +
+    coord_equal(xlim = range(comp$est, comp$value, na.rm = TRUE),
+                ylim = range(comp$est, comp$value, na.rm = TRUE)) +
+    scale_x_log10() +
+    scale_y_log10() +
+    facet_wrap(~Model) +
+    labs(title = "Total Outflow Comparison", x = "Estimated Outflow (m³)", y = "Model Outflow (m³)") +
+    theme_bw()
+  
+  ## --- Total rainfall ---
+  # comp <- wb |> 
+  #   dplyr::select(Date, model, rain) |>
+  #   dplyr::rename(est = rain) |>
+  #   dplyr::left_join(mod$rain, by = c("Date", "model")) |> 
+  #   dplyr::mutate(Model = toggle_models(model)) |> 
+  #   dplyr::filter(est > 0 & value > 0)
+  # p4 <- ggplot(comp) +
+  #   geom_point(aes(x = est, y = value)) +
+  #   geom_smooth(aes(x = est, y = value), method = "lm",
+  #               se = FALSE, color = "blue") +
+  #   geom_abline(slope = 1, intercept = 0, color = "red") +
+  #   coord_equal(xlim = range(comp$est, comp$value, na.rm = TRUE),
+  #               ylim = range(comp$est, comp$value, na.rm = TRUE)) +
+  #   scale_x_log10() +
+  #   scale_y_log10() +
+  #   facet_wrap(~Model) +
+  #   labs(title = "Total Rainfall Comparison", x = "Estimated Rainfall (m³)", y = "Model Rainfall (m³)") +
+  #   theme_bw()
+  
+  
+  evap_0 <- wb |> 
+    dplyr::filter(evap_m3 <= 0)
+  
+  mod_evap_0 <- mod$evap |> 
+    dplyr::filter(value <= 0)
+  
+  ## --- Total evaporation ---
+  comp <- wb |> 
+    dplyr::select(Date, model, evap_m3) |>
+    dplyr::rename(est = evap_m3) |>
+    dplyr::left_join(mod$evap, by = c("Date", "model")) |> 
+    dplyr::mutate(Model = toggle_models(model)) |> 
+    dplyr::filter(est > 0 & value > 0)
+  
+  p4 <- ggplot(comp) +
+    geom_point(aes(x = est, y = value)) +
+    geom_smooth(aes(x = est, y = value), method = "lm",
+                se = FALSE, color = "blue") +
+    geom_abline(slope = 1, intercept = 0, color = "red") +
+    coord_equal(xlim = range(comp$est, comp$value, na.rm = TRUE),
+                ylim = range(comp$est, comp$value, na.rm = TRUE)) +
+    # scale_x_log10() +
+    # scale_y_log10() +
+    facet_wrap(~Model) +
+    labs(title = "Total Evaporation Comparison", x = "Estimated Evaporation (
+m³)", y = "Model Evaporation (m³)") +
+    theme_bw()
+  
+  ## --- Surface temperature ---
+  comp <- wb |> 
+    dplyr::select(Date, model, Ts) |>
+    dplyr::rename(est = Ts) |>
+    dplyr::left_join(mod$ts, by = c("Date", "model")) |> 
+    dplyr::mutate(Model = toggle_models(model))
+  
+  p5 <- ggplot(comp) +
+    geom_point(aes(x = est, y = value)) +
+    geom_smooth(aes(x = est, y = value), method = "lm", se = FALSE, color = "blue") +
+    geom_abline(slope = 1, intercept = 0, color = "red") +
+    coord_equal(xlim = range(comp$est, comp$value, na.rm = TRUE),
+                ylim = range(comp$est, comp$value, na.rm = TRUE)) +
+    facet_wrap(~Model) +
+    labs(title = "Surface Temperature Comparison", x = "Estimated Surface Temp (°C)", y = "Model Surface Temp (°C)") +
+    theme_bw()
+  mae <- mean(abs(comp$est - comp$value), na.rm = TRUE)
+  
+  patchwork::wrap_plots(p2, p3, p4, p5, ncol = 2)
+}
