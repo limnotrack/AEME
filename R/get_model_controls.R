b705bb6195061eaeb8b3eeeffe6f412660a389ab
Tadhg Moore <tadhg.moore6@gmail.com>
2026-05-25 10:13:29 +1200

    Remove pH

--- R/get_model_controls.R
+++ R/get_model_controls.R
@@ -21,7 +21,7 @@ get_model_controls <- function(aeme = NULL, use_bgc = FALSE) {
     hyd_vars <- c("HYD_temp", "HYD_dens", "HYD_thmcln", "HYD_strat", "CHM_salt")
     light_vars <- c("RAD_par", "RAD_extc")
     bgc_vars <- c(
-      "CHM_oxy", "CHM_ph", 
+      "CHM_oxy",# "CHM_ph", 
       "PHS_frp", "PHS_dop", "PHS_pop", "PHS_pip", "PHS_tp",
       "NIT_amm", "NIT_nit", "NIT_don", "NIT_pon", "NIT_tn",
       "CAR_doc", "CAR_poc",
@@ -36,7 +36,8 @@ get_model_controls <- function(aeme = NULL, use_bgc = FALSE) {
       sel_vars <- c(hyd_vars, light_vars)
     }
     model_controls <- set_vars_sim(model_controls = model_controls,
-                                   vars_sim = sel_vars, simulate = TRUE)
+                                   vars_sim = sel_vars, simulate = TRUE, 
+                                   exclusive = TRUE)
     
   }
   return(model_controls)
