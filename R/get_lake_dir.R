77feb20b274ae16d7dc12922781346080ddcdbb3
Tadhg Moore <tadhg.moore6@gmail.com>
2026-09-15 09:38:47 +1200

    add fun for getting model_path

--- R/get_lake_dir.R
+++ R/get_lake_dir.R
@@ -4,6 +4,14 @@
 #'
 #' @return character; the directory of the lake model setup
 #' @export
+#' 
+#' @examples
+#' aeme_file <- system.file("extdata/aeme.rds", package = "AEME")
+#' aeme <- readRDS(aeme_file)
+#' path <- tempdir()
+#' model <- c("glm_aed")
+#' aeme <- build_aeme(aeme = aeme, model = model, path = path, ext_elev = 3)
+#' lake_dir <- get_lake_dir(aeme)
 #'
 
 get_lake_dir <- function(aeme, path) {
